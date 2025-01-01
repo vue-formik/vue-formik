77eafe2bd40e285eb1208486350ff2842a814428
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2025-01-01 23:51:04 +0545

    format fix
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- lib/index.ts
+++ lib/index.ts
@@ -3,9 +3,4 @@ import FormTextArea from "@/components/FormTextArea.vue";
 import FormTextField from "@/components/FormTextField.vue";
 import FormSelectField from "@/components/FormSelectField.vue";
 
-export {
-  FormTextArea,
-  FormTextField,
-  FormSelectField,
-  useFormik
-};
+export { FormTextArea, FormTextField, FormSelectField, useFormik };
