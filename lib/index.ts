9e7a71924f570bc4e8d049a7e2882ea2427a1800
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2025-01-02 01:52:06 +0545

    Chore: Namings (#5)
    * Enhance tests
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
    
    * Fixes
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
    
    * Revert tests
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,7 @@
 import useFormik from "@/composables/useFormik";
 import FormTextArea from "@/components/FormTextArea.vue";
-import FormTextField from "@/components/FormInput.vue";
+import FormInput from "@/components/FormInput.vue";
 import FormSelectField from "@/components/FormSelectField.vue";
 import FormContentEditable from "@/components/FormContentEditable.vue";
 
-const FormField = {
-  Input: FormTextArea,
-  TextArea: FormTextField,
-  Select: FormSelectField,
-  ContentEditable: FormContentEditable,
-};
-
-export { useFormik, FormTextArea, FormTextField, FormSelectField, FormContentEditable, FormField };
+export { useFormik, FormTextArea, FormInput, FormSelectField, FormContentEditable };
