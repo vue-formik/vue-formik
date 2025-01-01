30e684dc0c51ceb2ce33bd0aa0b04a69ab28bb81
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2025-01-02 01:51:16 +0545

    Revert tests
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- tests/unit/composables/useFormik.spec.ts
+++ tests/unit/composables/useFormik.spec.ts
@@ -1,187 +1,210 @@
-import { test, expect, describe, vi } from "vitest";
+import {test, expect, describe, vi} from 'vitest'
 import { useFormik } from "../../../lib";
-import { nextTick } from "vue";
+import {nextTick} from "vue";
 import { FormikHelpers } from "../../../lib/types";
 
-describe("useFormik", () => {
+describe("useFormik", async () => {
   const initialValues = {
     name: "Kiran",
     email: "kiran@parajuli.cc",
-    addresses: ["Home", "Office", ""],
-  };
+  }
   const emptyInitialValues = {
     name: "",
     email: "",
-    addresses: ["", "", ""],
-  };
-  const onSubmit = vi.fn((_values, { setSubmitting }) => {
-    setSubmitting(false); // Simulate the completion of the submission process
-  });
+  }
+  const onSubmit = vi.fn()
+
   const validationSchema = {
-    name: (value: string) => (!value ? "Name is required" : undefined),
-    email: (value: string) =>
-      !value ? "Email is required" : !value.includes("@") ? "Invalid email" : undefined,
-    addresses: (value: string[]) => {
-      if (!Array.isArray(value)) {
-        return "Addresses must be an array";
+    name: (value: string) => {
+      if (!value) {
+        return "Name is required"
       }
-
-      if (value.length === 0) {
-        return "Address is required";
-      }
-
-      const errs = []
-      for (let i = 0; i < value.length; i++) {
-        if (!value[i]) {
-          errs[i] = "Address is required";
-        }
-      }
-
-      return errs.length ? errs : undefined;
     },
-  };
+    email: (value: string) => {
+      if (!value) {
+        return "Email is required"
+      }
+      if (!value.includes("@")) {
+        return "Invalid email"
+      }
+    }
+  }
 
   describe("submit", () => {
-    test("should call onSubmit with initial values", () => {
-      const { handleSubmit } = useFormik({ initialValues, onSubmit });
-      handleSubmit({ preventDefault: vi.fn() });
-      expect(onSubmit).toHaveBeenCalledWith(initialValues, expect.any(Object));
-    });
-
+    test("submitting form should call onSubmit", () => {
+      const {handleSubmit} = useFormik({initialValues, onSubmit})
+      handleSubmit({preventDefault: vi.fn()})
+      expect(onSubmit).toHaveBeenCalled()
+      expect(onSubmit).toHaveBeenCalledWith(initialValues, expect.any(Object))
+    })
     test("'isSubmitting' should return true when form is submitting", async () => {
-      const onSubmit = vi.fn((_values, { setSubmitting }: FormikHelpers<typeof initialValues>) => {
-        expect(isSubmitting.value).toBe(true);
-        setSubmitting(false);
-        expect(isSubmitting.value).toBe(false);
-      });
+      const onSubmit = (_values: typeof initialValues, { setSubmitting }: FormikHelpers<typeof initialValues>) => {
+        expect(isSubmitting.value).toBe(true)
+        setSubmitting(false)
+        expect(isSubmitting.value).toBe(false)
+      }
 
-      const { isSubmitting, handleSubmit } = useFormik({
-        initialValues,
-        onSubmit,
-      });
+      const { isSubmitting, handleSubmit } = useFormik({ initialValues, onSubmit })
+      expect(isSubmitting.value).toBe(false)
 
-      expect(isSubmitting.value).toBe(false);
+      handleSubmit({ preventDefault: vi.fn() })
+    })
+  })
 
-      handleSubmit({ preventDefault: vi.fn() }); // Simulate form submission
+  test("should initialize form with initial values", () => {
+    const {values} = useFormik({initialValues, onSubmit})
+    expect(values).toMatchSnapshot()
+  })
 
-      await nextTick();
+  test("should validate form with validation schema", async () => {
+    const {
+      errors,
+      touched,
+      handleChange,
+      handleBlur
+    } = useFormik({ initialValues: emptyInitialValues, onSubmit, validationSchema })
+    expect(errors).toMatchSnapshot()
+    expect(touched).toMatchSnapshot()
 
-      expect(isSubmitting.value).toBe(false); // Verify submission state is reset
-    });
-  });
+    handleChange({ target: { name: "name", value: "Kiran" } })
+    handleBlur({ target: { name: "name" } })
 
-  describe("validation", () => {
-    test("should handle validation schema errors", async () => {
-      const { errors, handleBlur, handleChange } = useFormik({
-        initialValues: emptyInitialValues,
-        onSubmit,
-        validationSchema,
-      });
+    await nextTick()
 
-      expect(errors).toEqual({
-        name: "Name is required",
-        email: "Email is required",
-        addresses: [
-          "Address is required",
-          "Address is required",
-          "Address is required",
-        ]
-      });
+    expect(errors).toMatchSnapshot()
+    expect(touched).toMatchSnapshot()
 
-      handleChange({ target: { name: "name", value: "Kiran" } });
-      handleBlur({ target: { name: "name" } });
+    handleChange({ target: { name: "email", value: "kiran" } })
+    handleBlur({ target: { name: "email" } })
 
-      await nextTick();
+    await nextTick()
 
-      expect(errors.name).toBeUndefined();
-      expect(errors.email).toBe("Email is required");
-    });
+    expect(errors).toMatchSnapshot()
+    expect(touched).toMatchSnapshot()
 
-    test("should handle non-array values for addresses field", () => {
-      const { errors } = useFormik({
-        initialValues: { addresses: "Invalid Value" }, // Pass a non-array value
-        onSubmit,
-        validationSchema,
-      });
+    handleChange({ target: { name: "email", value: "kiran@test.com" } })
+    handleBlur({ target: { name: "email" } })
 
-      expect(errors.addresses).toBe("Addresses must be an array");
-    });
+    await nextTick()
 
-    test("should handle empty validation schema", () => {
-      const { errors } = useFormik({ initialValues, onSubmit });
-      expect(errors).toEqual({});
-    });
-  });
+    expect(errors).toMatchSnapshot()
+    expect(touched).toMatchSnapshot()
+  })
 
-  describe("state updates", () => {
-    test("should update touched fields", async () => {
-      const { setFieldTouched, touched } = useFormik({ initialValues, onSubmit });
-      setFieldTouched("name", true);
+  test("should set values", () => {
+    const { setValues, values } = useFormik({ initialValues, onSubmit })
+    setValues({ name: "Updated Name" })
+    expect(values).toMatchSnapshot()
+  })
 
-      await nextTick();
+  test("should set errors", () => {
+    const { setErrors, errors } = useFormik({ initialValues, onSubmit })
+    setErrors({ name: "Value must be less than 255 characters" })
+    expect(errors).toMatchSnapshot()
+  })
 
-      expect(touched.name).toBe(true);
-    });
+  test("should reset form", async () => {
+    const { reset, values, touched, setFieldTouched, setFieldValue } = useFormik({ initialValues, onSubmit })
 
-    test("should update field values", () => {
-      const { setFieldValue, values } = useFormik({ initialValues, onSubmit });
-      setFieldValue("name", "Updated Name");
+    setFieldTouched("name", true)
+    setFieldTouched("email", true)
+    setFieldValue("name", "Updated Name")
+    setFieldValue("email", "updated@mail.cc")
 
-      expect(values.name).toBe("Updated Name");
-    });
+    await nextTick()
 
-    test("should reset form state", async () => {
-      const {
-        reset,
-        values,
-        touched,
-        setFieldValue,
-        setFieldTouched
-      } = useFormik({ initialValues, onSubmit });
+    expect(values).toMatchSnapshot()
+    expect(touched).toMatchSnapshot()
 
-      setFieldValue("name", "New Name");
-      setFieldTouched("name", true);
+    reset()
 
-      await nextTick();
+    await nextTick()
 
-      reset();
+    expect(values).toMatchSnapshot()
+    expect(touched).toMatchSnapshot()
+  })
 
-      await nextTick();
+  test("should set field value", () => {
+    const { setFieldValue, values } = useFormik({ initialValues, onSubmit })
+    setFieldValue("name", "Updated Name")
+    expect(values).toMatchSnapshot()
+  })
 
-      expect(values).toEqual(initialValues);
-      expect(touched).toEqual({});
-    });
-  });
+  test("should set field touched", () => {
+    const { setFieldTouched, touched } = useFormik({ initialValues, onSubmit })
+    setFieldTouched("name", true)
+    expect(touched).toMatchSnapshot()
+  })
+
+  describe("status", () => {
+    test("'isValid' should return true when form is valid", async () => {
+      const { isValid, handleChange, handleBlur } = useFormik({ initialValues, onSubmit, validationSchema })
+      expect(isValid.value).toBe(true)
+
+      handleChange({ target: { name: "email", value: "Kiran" } })
+      handleBlur({ target: { name: "email" } })
+
+      await nextTick()
+
+      expect(isValid.value).toBe(false)
+    })
+    test("'isDirty' should return true when form is dirty", async () => {
+      const { isDirty, handleChange, handleBlur, reset } = useFormik({ initialValues, onSubmit, validationSchema })
+      expect(isDirty.value).toBe(false)
+
+      handleChange({ target: { name: "email", value: "Kiran" } })
+      handleBlur({ target: { name: "email" } })
+
+      await nextTick()
+
+      expect(isDirty.value).toBe(true)
+
+      reset()
+
+      await nextTick()
+
+      expect(isDirty.value).toBe(false)
+    })
+  })
 
   describe("field errors", () => {
-    test("should return field error if present", async () => {
-      const { getFieldError, setFieldValue, setFieldTouched } = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-      });
+    test("should return field error", async () => {
+      const { getFieldError, setFieldValue, setFieldTouched } = useFormik({ initialValues, onSubmit, validationSchema })
+      await nextTick()
+      expect(getFieldError("name")).toBe("")
 
-      setFieldValue("name", "");
-      setFieldTouched("name", true);
+      setFieldValue("name", "")
+      setFieldTouched("name", true)
 
-      await nextTick();
+      await nextTick()
 
-      expect(getFieldError("name")).toBe("Name is required");
-    });
+      expect(getFieldError("name")).toBe("Name is required")
+    })
 
-    test("should return true for hasFieldError if error exists", async () => {
-      const { hasFieldError, setFieldValue, setFieldTouched } = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-      });
+    test("should return true if field has error", async () => {
+      const { hasFieldError, setFieldValue, setFieldTouched } = useFormik({ initialValues, onSubmit, validationSchema })
+      await nextTick()
+      expect(hasFieldError("name")).toBe(false)
 
-      setFieldValue("name", "");
-      setFieldTouched("name", true);
+      setFieldValue("name", "")
+      setFieldTouched("name", true)
 
-      await nextTick();
+      await nextTick()
 
-      expect(hasFieldError("name")).toBe(true);
-    });
-  });
-});
+      expect(hasFieldError("name")).toBe(true)
+    })
+
+    test("should return false if field has no error", async () => {
+      const { hasFieldError, setFieldValue, setFieldTouched } = useFormik({ initialValues, onSubmit, validationSchema })
+      await nextTick()
+      expect(hasFieldError("name")).toBe(false)
+
+      setFieldValue("name", "Kiran")
+      setFieldTouched("name", true)
+
+      await nextTick()
+
+      expect(hasFieldError("name")).toBe(false)
+    })
+  })
+})
