d50e64bbf5e638f17d54faaf2a449368f8fb09bb
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2025-01-12 19:07:41 +0545

    feat: FormikForm component (#7)
    * feat: FormikForm component
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
    
    * Lint & format fixes
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- lib/index.ts
+++ lib/index.ts
@@ -3,5 +3,6 @@ import FormTextArea from "@/components/FormTextArea.vue";
 import FormInput from "@/components/FormInput.vue";
 import FormSelectField from "@/components/FormSelectField.vue";
 import FormContentEditable from "@/components/FormContentEditable.vue";
+import FormikForm from "@/components/FormikForm.vue";
 
-export { useFormik, FormTextArea, FormInput, FormSelectField, FormContentEditable };
+export { useFormik, FormTextArea, FormInput, FormSelectField, FormContentEditable, FormikForm };
