3d219cd69a7f7b9cd449cfa91220abae1b750ac6
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2025-01-25 18:15:06 +0545

    useFieldArray composable
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

---
+++ lib/composables/useFieldArray.ts
@@ -0,0 +1,50 @@
+import { Formik } from "@/types";
+import { inject } from "vue";
+import { array } from "yup";
+
+// overloading function for two different return types
+function useFieldArray(formik: Formik): {
+  push: (field: string, value: unknown) => void;
+  pop: (field: string, index: number) => void
+};
+function useFieldArray(): {
+  push: (field: string, value: unknown) => void;
+  pop: (field: string, index: number) => void
+} | undefined;
+
+function useFieldArray(formik?: Formik) {
+  const injectedFormik: Formik | undefined = inject("formik");
+  const fk = formik || injectedFormik;
+
+  if (!fk) {
+    console.error("useFieldArray must be used within a Formik component");
+    return undefined;
+  }
+
+  const push = (field: string, value: unknown) => {
+    if (Array.isArray(fk.values[field])) {
+      fk.setFieldValue(field, [...fk.values[field], value]);
+    } else {
+      console.warn(`Field ${field} is not an array`);
+    }
+  };
+
+  const pop = (field: string, index: number) => {
+    const fieldValue = fk.getFieldValue(field);
+    if (Array.isArray(fieldValue)) {
+      if (index < 0 || index >= fieldValue.length) {
+        console.warn(`Index ${index} out of bounds`);
+        return;
+      }
+
+      fk.setFieldValue(field, fieldValue.filter((_, i) => i !== index));
+      fk.setFieldTouched(`${field}[${index}]`, false);
+    } else {
+      console.warn(`Field ${field} is not an array`);
+    }
+  };
+
+  return { push, pop };
+}
+
+export default useFieldArray;
---
+++ tests/unit/composables/useFieldArray.spec.ts
@@ -0,0 +1,133 @@
+import { vi, describe, test, expect } from "vitest";
+import { useFieldArray, useFormik } from "@/index";
+
+describe("useFieldArray composable", () => {
+  test("should return null if formik is not provided", () => {
+    const ufa = useFieldArray()
+    expect(ufa).toBeUndefined()
+  })
+
+  describe("push method", () => {
+    test.each([
+      {
+        name: "should add a value to an array field",
+        initialValues: { names: ["John", "Doe"] },
+        value: "Jane",
+        expected: ["John", "Doe", "Jane"],
+      },
+      {
+        name: "should add a value to an empty array field",
+        initialValues: { names: [] },
+        value: "Jane",
+        expected: ["Jane"],
+      },
+      {
+        name: "should add an object to an object array field",
+        initialValues: { names: [{ name: "John" }] },
+        value: { name: "Doe" },
+        expected: [{ name: "John" }, { name: "Doe" }],
+      },
+      {
+        name: "should add an object to an empty object array field",
+        initialValues: { names: [] },
+        value: { name: "Doe" },
+        expected: [{ name: "Doe" }],
+      }
+    ])("$name", ({ initialValues, value, expected }) => {
+      const fk = useFormik({ initialValues })
+      const ufa = useFieldArray(fk)
+      ufa.push("names", value)
+      expect(fk.values.names).toEqual(expected)
+    })
+
+    test("should not add a value to a non-array field", () => {
+      console.warn = vi.fn()
+      const fk = useFormik({
+        initialValues: { name: "John" },
+      })
+      const ufa = useFieldArray(fk)
+      ufa.push("name", "Jane")
+      expect(fk.values.name).toBe("John")
+      expect(console.warn).toHaveBeenCalled()
+      expect(console.warn).toHaveBeenCalledWith("Field name is not an array")
+    })
+  })
+
+  describe("pop method", () => {
+    test.each([
+      {
+        name: "should remove a value from an array field",
+        initialValues: { names: ["John", "Doe", "Jane"] },
+        index: 1,
+        expected: ["John", "Jane"],
+      },
+      {
+        name: "should remove the only value from an array field",
+        initialValues: { names: ["John"] },
+        index: 0,
+        expected: [],
+      },
+      {
+        name: "should remove a object from an object array field",
+        initialValues: { names: [{ name: "John" }, { name: "Doe" }] },
+        index: 1,
+        expected: [{ name: "John" }],
+      }, {
+        name: "should remove a object from an object array field",
+        initialValues: { names: [{ name: "John" }, { name: "Doe" }] },
+        index: 0,
+        expected: [{ name: "Doe" }],
+      }
+    ])("$name", ({ initialValues, index, expected }) => {
+      const fk = useFormik({ initialValues })
+      const ufa = useFieldArray(fk)
+      ufa.pop("names", index)
+      expect(fk.values.names).toEqual(expected)
+    })
+
+    test("should not remove a value from a non-array field", () => {
+      console.warn = vi.fn()
+      const fk = useFormik({
+        initialValues: { name: "John" },
+      })
+      const ufa = useFieldArray(fk)
+      ufa.pop("name", 0)
+      expect(fk.values.name).toBe("John")
+      expect(console.warn).toHaveBeenCalled()
+      expect(console.warn).toHaveBeenCalledWith("Field name is not an array")
+    })
+
+    test("index out of bounds", () => {
+      console.warn = vi.fn()
+      const fk = useFormik({
+        initialValues: { names: ["John", "Doe"] },
+      })
+      const ufa = useFieldArray(fk)
+      ufa.pop("names", 2)
+      expect(fk.values.names).toEqual(["John", "Doe"])
+      expect(console.warn).toHaveBeenCalled()
+      expect(console.warn).toHaveBeenCalledWith("Index 2 out of bounds")
+    })
+
+    test("index out of bounds (-ve index)", () => {
+      console.warn = vi.fn()
+      const fk = useFormik({
+        initialValues: { names: ["John", "Doe"] },
+      })
+      const ufa = useFieldArray(fk)
+      ufa.pop("names", -1)
+      expect(fk.values.names).toEqual(["John", "Doe"])
+      expect(console.warn).toHaveBeenCalled()
+      expect(console.warn).toHaveBeenCalledWith("Index -1 out of bounds")
+    })
+  })
+
+  test("should return push and pop methods", () => {
+    const fk = useFormik({ initialValues: { names: ["John", "Doe"] } })
+    const ufa = useFieldArray(fk)
+    expect(ufa).toEqual({
+      push: expect.any(Function),
+      pop: expect.any(Function),
+    })
+  })
+})
